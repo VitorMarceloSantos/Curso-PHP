4728dc7513c58ce585f98e08196c585899f48a16
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-06-06 11:42:20 -0300

    Curso PHP

---
+++ Curso-em-Video/Modulo-01/Exercicio-02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Formulários</title>
+</head>
+
+<body>
+  <header>
+    <h1>Apresente-se</h1>
+  </header>
+
+  <form action="cad.php" method="get">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="idnome">
+    <label for="sobrenome">Sobrenome</label>
+    <input type="text" name="sobrenome" id="idsobrenome">
+    <input type="submit" value="Enviar">
+  </form>
+
+  <!-- <?php
+  echo "Hello World! - Exercicio 01 \u{1F30E}";
+  ?> -->
+</body>
+
+</html>
---
+++ html/cad.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulário</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Resultado do Formulário</h1>
+  </header>
+  <?php
+  // var_dump($_REQUEST) -> mostra o que está sendo recebido no metodo(GET, POST, COOKIES)
+  // var_dump($_GET) -> mostra o que está sendo recebido no metodo(GET)
+    $nome = $_GET["nome"] ?? "Sem nome"; // $_GET é uma variável global(possui todos os dados vindo do request)
+    $sobrenome = $_GET["sobrenome"] ?? "Sem sobrenome"; // ?? caso não receba um valor vai ter um valor default
+    echo "<p>É um prazer te conhercer $nome $sobrenome.</p>";
+  ?>
+</body>
+
+</html>
